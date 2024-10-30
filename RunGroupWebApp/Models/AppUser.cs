46c5b70f98cebe2230121139a458c7ad2f05695e
Sharpsharky <maciej.jasielewicz@gmail.com>
2024-10-30 19:44:17 +0100

    Add project files.

---
+++ RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroupWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Milage { get; set; }
+        public Address? Address { get; set; }
+    }
+}
